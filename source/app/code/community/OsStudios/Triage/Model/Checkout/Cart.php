554240a37ed10d4da28e8926dc9436fcf40b50a9
Tiago Sampaio <tiago.sampaio@osstudios.com.br>
2012-08-14 15:50:33 -0300

    Corrected logic into the block process for offline customers.

--- source/app/code/community/OsStudios/Triage/Model/Checkout/Cart.php
+++ source/app/code/community/OsStudios/Triage/Model/Checkout/Cart.php
@@ -27,21 +27,22 @@ class OsStudios_Triage_Model_Checkout_Cart extends Mage_Checkout_Model_Cart
         if(Mage::helper('triage')->isEnabled()) {
             
             $collection = $productInfo->getCategoryCollection();
-            $allowed = explode(',', $this->_getConfig('categories'));
+            $blocked = explode(',', $this->_getConfig('categories'));
             
             foreach( $collection as $category ) {
-                
-                if(!Mage::helper('customer')->isLoggedin()) {
-                    $message = Mage::helper('triage')->__('To purchase this product you need to login in the system.');
-                    Mage::throwException($message);
-                } else {
-                    
-                    $customer = Mage::getSingleton('customer/session')->getCustomer();
-                    $allowed_groups = explode(',', $this->_getConfig('groups'));
-                    
-                    if( in_array($category->getEntityId(), $allowed) && !in_array($customer->getGroupId(), $allowed_groups)) {
-                        $message = Mage::helper('triage')->__('This product cannot be added to your cart.');
+                if(in_array($category->getEntityId(), $blocked)) {
+                    if(!Mage::helper('customer')->isLoggedin()) {
+                        $message = Mage::helper('triage')->__('To purchase this product you need to login in the system.');
                         Mage::throwException($message);
+                    } else {
+
+                        $customer = Mage::getSingleton('customer/session')->getCustomer();
+                        $allowed_groups = explode(',', $this->_getConfig('groups'));
+
+                        if(!in_array($customer->getGroupId(), $allowed_groups)) {
+                            $message = Mage::helper('triage')->__('This product cannot be added to your cart.');
+                            Mage::throwException($message);
+                        }
                     }
                 }
             }
